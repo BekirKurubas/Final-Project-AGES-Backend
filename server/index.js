84ece4a512f318a0088dfe66b4c2f5a7df95a203
Bekir Kurubas <bekirkurubas6767@gmail.com>
2024-06-01 10:30:54 +0200

    netlify has added to index.js

--- server/index.js
+++ server/index.js
@@ -10,10 +10,15 @@ import { examRouter } from './router/exam.router.js';
 import { imageRouter } from './router/image.router.js';
 
 const app = express();
-const port = 4000;
+const port = process.env.SERVER_PORT || 4000;
 
-app.use(morgan(process.env.ACCESS_LOG_FORMAT))
-app.use(cors());
+app.use(morgan(process.env.ACCESS_LOG_FORMAT));
+const corsOptions = {
+  origin: 'https://agestest.netlify.app/',
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -31,7 +36,6 @@ app.use((err, req, res, next) => {
 
 process.on('uncaughtException', err => {
   process.exit(0);
-})
-
+});
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
